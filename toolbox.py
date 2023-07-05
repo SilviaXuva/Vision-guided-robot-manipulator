512dd7576cc2dd26120d48768236e08f948e949f
Silvia Xuva <silviaxuva@gmail.com>
2023-07-05 14:10:44 -0300

    Update joint trajectory

--- toolbox.py
+++ toolbox.py
@@ -1,5 +1,6 @@
 from Data.targets import target3 as target
 from spatialmath import SE3
+import roboticstoolbox as rtb
 from Toolbox.LBR_iiwa_DH import LBR_iiwa
 
 robot = LBR_iiwa()
@@ -7,5 +8,7 @@ robot = LBR_iiwa()
 T0 = robot.fkine(robot.q)
 T1 = SE3.Trans(target[0][:3])*SE3.Rx(target[0][3])*SE3.Ry(target[0][4])*SE3.Rz(target[0][5])
 
-jtraj = robot.jtraj(T0, T1, robot.t)
+q0 = robot.q
+q1 = robot.ikine_LM(T1, q0).q
+jtraj = rtb.jtraj(q0, q1, robot.t)
 robot.plot(jtraj.q)
