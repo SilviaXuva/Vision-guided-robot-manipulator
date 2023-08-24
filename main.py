fef2fd5f1ed937c79998f0867717593141374f6d
Silvia Xuva <silviaxuva@gmail.com>
2023-08-24 02:05:17 -0300

    Change scene

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import roboticstoolbox as rtb
 from settings import Settings
 
 robot = LBR_iiwa(Settings.q0)
-robot.Coppelia = RobotSimulator(robot, scene = 'robot_vel_dynamic.ttt', drawing = True, gripper = True, vision = False)
+robot.Coppelia = RobotSimulator(robot, drawing = True, gripper = True, vision = False)
 
 robot.Coppelia.start()
 
